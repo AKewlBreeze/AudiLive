1eac305f48b9a0b43ceb591169ec432d8a5a2253
Breana Knight <Akewlbreeze.bc@gmail.com>
2017-05-30 15:07:49 -0600

    removed a redundant file. Crudely drawing based off face position.

--- libraries/visualControl.js
+++ libraries/visualControl.js
@@ -3,15 +3,43 @@ function setup() {
   createCanvas(400, 400);
 }
 
-function draw() {
-  if (mouseIsPressed) {
+// function draw() {
+//   if (mouseIsPressed) {
+//     fill(0);
+//   } else {
+//     fill(255);
+//   }
+//   ellipse(mouseX, mouseY, 80, 80);
+//
+// }
+
+
+let draw = (x, y) => {
+  if (x > 100) {
     fill(0);
   } else {
     fill(255);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  let mouseX = x;
+  let mouseY = y;
+  ellipse(mouseX, mouseY, 280, 280);
 
 }
+
+
+
+let drawing = (x, y)=>{
+  if(x > 100){
+    console.log(x)
+  } else {
+    console.log('nope')
+  }
+  }
+
+
+
+
+
 
 // <!--  tracking.js script-->
       window.onload = function() {
@@ -31,13 +59,15 @@ function draw() {
           context.clearRect(0, 0, canvas.width, canvas.height);
 
           event.data.forEach(function(rect) {
-            console.log(rect, 1);
+            // console.log(rect, 1);
             context.strokeStyle = '#a64ceb';
             context.strokeRect(rect.x, rect.y, rect.width, rect.height);
             context.font = '11px Helvetica';
             context.fillStyle = "#fff";
             context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
             context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+            draw(rect.x, rect.y);
+            // drawing(rect.x, rect.y);
             // console.log(rect.x, rect.y)
           });
         });
